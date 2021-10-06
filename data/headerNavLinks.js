e1fd813f177a59bae293ab16f29f3d4a7e43d6c5
DiamondSat <91375220+DiamondSat@users.noreply.github.com>
2021-10-06 03:12:04 +0300

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Latest' },
   { href: '/tags', title: 'Academics' },
   { href: '/projects', title: 'Up to today' },
-  { href: '/about', title: 'About' },
+  { href: '/about', title: 'About me' },
 ]
 
 export default headerNavLinks
