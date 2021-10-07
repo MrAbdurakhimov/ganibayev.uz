f7a84271f7e1aa2e1fb58a2780f14206d884028a
DiamondSat <91375220+DiamondSat@users.noreply.github.com>
2021-10-07 14:38:12 +0300

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,6 +1,6 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
+    title: 'Rugyby 2021',
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
@@ -8,20 +8,14 @@ const projectsData = [
     href: 'https://www.google.com',
   },
   {
-    title: 'Polo Independence Day 2022',
+    title: 'Polo Independence Day 2021',
     description: `skvskvmskvk sdmlsdmlsm sdofmsmf sdofmomosd sdofmsfml sdfmlsdflsdlfmsl`,
     imgSrc: '/static/images/google.png',
     href: 'https://github.com',
   },
   {
-    title: 'Polo Independence Day 2021',
+    title: 'Laser Run 2021',
     description: `sjdjbjx cdjscj dskcnsj csjdcj scsbcj sdj`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://github.com',
-  },
-  {
-    title: 'A Real Engine',
-    description: `oremsdjk s dvjsd fv skdjf vjksd fjkv sdjkfv jksd fvjk sdkjfv sjkdf vjks djkv sjkdf vjks djkfv jksd fvjk sdjkf`,
     imgSrc: '/static/images/google.png',
     href: 'https://github.com',
   },
@@ -30,14 +24,6 @@ const projectsData = [
     description: `sjdjbjx cdjscj dskcnsj csjdcj scsbcj sdj`,
     imgSrc: '/static/images/google.png',
     href: 'https://github.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
   },
 ]
 
