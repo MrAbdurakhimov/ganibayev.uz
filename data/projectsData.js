b76573d793002306cb824025c8866d2bb8ae3ed9
DiamondSat <91375220+DiamondSat@users.noreply.github.com>
2021-10-07 16:23:01 +0300

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,23 +1,23 @@
 const projectsData = [
   {
-    title: 'Rugyby 2021',
+    title: 'Rugby 2021',
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    href: 'https://instagram.com/asiarugbylive?utm_medium=copy_link',
   },
   {
     title: 'Polo Independence Day 2021',
     description: `skvskvmskvk sdmlsdmlsm sdofmsmf sdofmomosd sdofmsfml sdfmlsdflsdlfmsl`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://github.com',
+    imgSrc: 'https://wallpaperskingdom.com/sites/default/files/styles/225x120/public/polo-ralph-lauren-logo-wallpaper-40881-8976895.png?itok=-emLu8TG',
+    href: 'https://instagram.com/uzpolo?utm_medium=copy_link',
   },
   {
     title: 'Laser Run 2021',
     description: `sjdjbjx cdjscj dskcnsj csjdcj scsbcj sdj`,
     imgSrc: '/static/images/google.png',
-    href: 'https://github.com',
+    href: 'https://instagram.com/mpf.uz?utm_medium=copy_link',
   },
   {
     title: 'TED-ED club with Lyceum',
